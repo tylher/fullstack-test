7820cefc929e5713fdb1d1a8df052d9f398b5dc8
Damola <tylher123@gmail.com>
2023-12-18 21:06:03 +0100

    change base url to remote url

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://fullstack-test-e1sd.onrender.com/api";
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://fullstack-test-e1sd.onrender.com/api";
+// export const BASE_URL = "http://localhost:8080/api";
 
 export const organizeData = (data) => {
   return Object.entries(
